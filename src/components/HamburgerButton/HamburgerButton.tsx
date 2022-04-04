65c01bbfb7cc9a7f73ee33f2e19441837ef54b16
merinav <81377750+merinav@users.noreply.github.com>
2022-04-04 12:06:10 +0300

    feature/echo-7-hamburger-button First Commit

---
+++ src/components/HamburgerButton/HamburgerButton.tsx
@@ -0,0 +1,19 @@
+import { parseMultipleClassNames } from 'utils/theme/styleUtils';
+
+import styles from './HamburgerButton.module.css';
+
+type HamburgerButtonProps = {
+  isActive: boolean;
+  onClick?: () => void;
+};
+
+const HamburgerButton = ({ isActive, onClick }: HamburgerButtonProps): JSX.Element => {
+  return (
+    <div className={isActive ? parseMultipleClassNames([styles.hamburger, styles.active]) : styles.hamburger} onClick={onClick}>
+      <span className={styles.bar}></span>
+      <span className={styles.bar}></span>
+      <span className={styles.bar}></span>
+    </div>
+  );
+};
+export default HamburgerButton;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
+import HamburgerButton from 'components/HamburgerButton/HamburgerButton';
 import { MyMoviesLogo } from 'components/Icons';
 
 import styles from './Header.module.css';
 
-const Header = () => {
+const Header = (): JSX.Element => {
   return (
     <header className={styles.header}>
       <MyMoviesLogo className={styles.icon} />
+      <HamburgerButton isActive={true} />
     </header>
   );
 };
