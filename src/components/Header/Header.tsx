5728bb08a47fa021123af9d86e63110ec9bf136c
merinav <81377750+merinav@users.noreply.github.com>
2022-04-10 19:50:57 +0300

    feature/echo-9-sidebar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,29 @@
 import HamburgerButton from 'components/HamburgerButton/HamburgerButton';
 import { MyMoviesLogo } from 'components/Icons';
+import Sidebar from 'components/Sidebar/Sidebar';
+import useMediaQuery from 'hooks/useMediaQuery';
+import { useState } from 'react';
 
 import styles from './Header.module.css';
 
 const Header = (): JSX.Element => {
+  const isMobile = useMediaQuery('(max-width: 780px)');
+
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  let showHamburgerButtonTrue = false;
+
+  if (isMobile) {
+    if (showSidebar) {
+      showHamburgerButtonTrue = true;
+    }
+  }
+
   return (
     <header className={styles.header}>
       <MyMoviesLogo className={styles.icon} />
-      <HamburgerButton isActive={true} />
+      {isMobile && <HamburgerButton isActive={showHamburgerButtonTrue} onClick={() => setShowSidebar(!showSidebar)} />}
+      {isMobile && showSidebar && <Sidebar onBackDropClick={() => setShowSidebar(!showSidebar)} />}
     </header>
   );
 };
