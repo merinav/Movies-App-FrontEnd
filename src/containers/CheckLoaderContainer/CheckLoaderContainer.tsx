b7520fdc789ea16b43ec9013bcd7b5f9bdac8d4d
merinav <81377750+merinav@users.noreply.github.com>
2022-05-10 10:03:51 +0300

    feature/echo-12-movies-request

--- src/containers/CheckLoaderContainer/CheckLoaderContainer.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Loader from 'components/Loader/Loader';
-
-const CheckLoaderContainer = (): JSX.Element => {
-  return <Loader />;
-};
-
-export default CheckLoaderContainer;
--- src/navigation/MainRouter.tsx
+++ src/navigation/MainRouter.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import MovieListContainer from 'containers/MovieListContainer/MovieListContainer';
-import CheckLoaderContainer from 'containers/CheckLoaderContainer/CheckLoaderContainer';
 
 import { RouteKeys } from './routes';
 
@@ -9,7 +8,7 @@ const MainRouter = (): JSX.Element => {
   return (
     <div>
       <Routes>
-        <Route element={<CheckLoaderContainer />} path={RouteKeys.Index} />
+        <Route element={<MovieListContainer />} path={RouteKeys.Index} />
         <Route element={<MovieListContainer />} path={RouteKeys.Movies} />
       </Routes>
     </div>
