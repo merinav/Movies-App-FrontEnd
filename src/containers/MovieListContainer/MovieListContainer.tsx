605a7c5918c172a042fc2e398f7bfee19ac084a6
merinav <81377750+merinav@users.noreply.github.com>
2022-05-17 14:43:50 +0300

    Movie List

--- src/containers/MovieListContainer/MovieListContainer.tsx
+++ src/containers/MovieListContainer/MovieListContainer.tsx
@@ -1,6 +1,4 @@
-//import React from 'react';
 import { getMovies } from 'api/movies/movies';
-//import type { Movie } from 'api/movies/types';
 import { useQuery } from 'react-query';
 import Loader from 'components/Loader/Loader';
 
@@ -9,15 +7,10 @@ import styles from './MovieListContainer.module.css';
 
 const MovieListContainer = (): JSX.Element => {
   const { isLoading, data } = useQuery('movies', getMovies);
-  const movie0 = data?.movies[0];
 
-  //const iterable = [data?.movies];
-  //const movies : Movie = iterable;
-  //const test = [ iterable?.forEach((element) => {movie})];
+  const iterable = data?.movies;
 
-  const moviesArray = [movie0, movie0, movie0];
-
-  const movieCards = moviesArray.map((movie) => (
+  const movieCards = iterable?.map((movie) => (
     <MovieCard imageUrl={movie?.posterPath} key={movie?.movieId} releaseDate={movie?.releaseDate} title={movie?.title} voteAverage={movie?.voteAverage} />
   ));
 
