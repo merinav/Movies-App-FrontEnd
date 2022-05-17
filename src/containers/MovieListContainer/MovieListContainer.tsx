2b166ee7a20bfb84bddfceb15839c5283c08aa3e
merinav <81377750+merinav@users.noreply.github.com>
2022-05-17 11:16:34 +0300

    Array of data results implementation

--- src/containers/MovieListContainer/MovieListContainer.tsx
+++ src/containers/MovieListContainer/MovieListContainer.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+//import React from 'react';
 import { getMovies } from 'api/movies/movies';
+//import type { Movie } from 'api/movies/types';
 import { useQuery } from 'react-query';
 import Loader from 'components/Loader/Loader';
 
@@ -8,17 +9,23 @@ import styles from './MovieListContainer.module.css';
 
 const MovieListContainer = (): JSX.Element => {
   const { isLoading, data } = useQuery('movies', getMovies);
-  const movie = data?.movies[0];
+  const movie0 = data?.movies[0];
+
+  //const iterable = [data?.movies];
+  //const movies : Movie = iterable;
+  //const test = [ iterable?.forEach((element) => {movie})];
+
+  const moviesArray = [movie0, movie0, movie0];
+
+  const movieCards = moviesArray.map((movie) => (
+    <MovieCard imageUrl={movie?.posterPath} key={movie?.movieId} releaseDate={movie?.releaseDate} title={movie?.title} voteAverage={movie?.voteAverage} />
+  ));
 
   if (isLoading) {
     return <Loader />;
   }
 
-  return (
-    <div className={styles.movieListContainer}>
-      <MovieCard imageUrl={movie?.posterPath} releaseDate={movie?.releaseDate} title={movie?.title} voteAverage={movie?.voteAverage} />
-    </div>
-  );
+  return <div className={styles.movieListContainer}>{movieCards}</div>;
 };
 
 export default MovieListContainer;
